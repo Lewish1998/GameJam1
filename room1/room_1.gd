5e3fdad4bf02c0be5d99aa0e4b585d5d97720017
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2024-02-25 13:02:53 +0000

    d

--- room1/room_1.gd
+++ room1/room_1.gd
@@ -4,7 +4,7 @@ var rng = RandomNumberGenerator.new()
 var flicker = 3
 
 var sound_player := AudioStreamPlayer.new()
-
+var transition_scene = load("res://UI/Transition.tscn").instantiate()
 func _ready():
 	# Add the AudioStreamPlayer as a child to the player node
 	add_child(sound_player)
@@ -12,38 +12,39 @@ func _ready():
 	sound_player.stream = sound_effect
 	sound_player.play()
 
-func _process(delta):
-	var my_random_number = rng.randf_range(0.0, 1.7)
-	$PointLight2D.energy = lerp($PointLight2D.energy, my_random_number, flicker*delta)
+	transition_scene.disabled = true
+		
 	
-	if GameObjects.num_objects >= 3:
-		print("Next room")
-		GameObjects.num_objects = 0
-		GameObjects.current_room = "Pond"
-		get_tree().change_scene_to_file("res://room2/room_2.tscn")
-		
-		
+#
 #func _process(delta):
-	## Existing process logic
 	#var my_random_number = rng.randf_range(0.0, 1.7)
 	#$PointLight2D.energy = lerp($PointLight2D.energy, my_random_number, flicker*delta)
 	#
 	#if GameObjects.num_objects >= 3:
+		#print("Next room")
 		#GameObjects.num_objects = 0
 		#GameObjects.current_room = "Pond"
-		#show_transition()
-#
-#func show_transition():
-	#var transition_scene = load("res://UI/Transition.tscn").instance()
+		#get_tree().change_scene_to_file("res://room2/room_2.tscn")
+		#
+		#
+func _process(delta):
+	# Existing process logic
+	var my_random_number = rng.randf_range(0.0, 1.7)
+	$PointLight2D.energy = lerp($PointLight2D.energy, my_random_number, flicker*delta)
+	
+	if GameObjects.num_objects >= 3:
+		GameObjects.num_objects = 0
+		GameObjects.current_room = "Pond"
+		show_transition()
+
+func show_transition():
+	#var transition_scene = load("res://UI/Transition.tscn").instantiate()
 	#add_child(transition_scene)
 	#transition_scene.get_node("AnimationPlayer").connect("animation_finished", self, "_on_transition_finished")
-#
-#func _on_transition_finished():
-	#get_tree().change_scene("res://room2/room_2.tscn")
+	
+	transition_scene.show()
+	transition_scene.get_node("transition")
 
-		
-		# show text box 
-		# show the duck ?
-		# wait for plater input
-		# transition to next scene
+func _on_transition_finished():
+	get_tree().change_scene("res://room2/room_2.tscn")
 
